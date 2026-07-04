b1a1b1b4462b2d87ceea81af383c7c3c1c80aed2
Oleksii Baranovskyi <c0ldsh0t1718@gmail.com>
2026-07-04 11:11:24 +0200

    lesson_2 home work

---
+++ lesson_2/main.py
@@ -0,0 +1,57 @@
+number = int(input("Enter a number: "))
+result = number * number
+
+print(result)
+
+number1 = 2
+number2 = 4
+number3 = 6
+result = (number1 + number2 + number3) / 3
+
+print(result)
+
+minutes = 135
+hours = minutes // 60
+remaining_minutes = minutes % 60
+
+print(hours, "часа", remaining_minutes, "минут")
+
+price = 1000
+discount = price * 15 // 100
+last_price = price - discount
+
+print(discount)
+print(last_price)
+
+number = 347
+result = number % 10
+
+print(result)
+
+length = 5
+width = 3
+result = (length  + width ) * 2
+print(result)
+
+# or
+
+length = 5
+width = 3
+result = length * 2 + width * 2
+print(result)
+
+number1 = 1234
+result = number1 // 1000
+print(result)
+number2 = 1234
+result = number2 // 100 % 10
+print(result)
+number3 = 1234
+result = number3 // 10 % 10
+print(result)
+number4 = 1234
+result = number4 % 10
+print(result)
+
+
+
