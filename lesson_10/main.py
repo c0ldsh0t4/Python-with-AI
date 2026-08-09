17a9e456409ff9f164175f72c04127bed40a6531
Oleksii Baranovskyi <c0ldsh0t1718@gmail.com>
2026-08-09 10:20:04 +0200

    home work 10.3 is done

--- lesson_10/main.py
+++ lesson_10/main.py
@@ -1,32 +1,20 @@
 #Home work 10.1
-def pow(x):
-   return x ** 2
-
-def some_gen(begin, end, func):
-   yield begin
-
-   for i in range(end -1):
-      begin = func(begin)
-      yield begin
-
-from inspect import isgenerator
-
-gen = some_gen(2, 4, pow)
-assert isgenerator(gen) == True, 'Test1'
-assert list(gen) == [2, 4, 16, 256], 'Test2'
-print('OK')
-
-
-
-
-
-
-
-
-
-
-
-
+# def pow(x):
+#    return x ** 2
+#
+# def some_gen(begin, end, func):
+#    yield begin
+#
+#    for i in range(end -1):
+#       begin = func(begin)
+#       yield begin
+#
+# from inspect import isgenerator
+#
+# gen = some_gen(2, 4, pow)
+# assert isgenerator(gen) == True, 'Test1'
+# assert list(gen) == [2, 4, 16, 256], 'Test2'
+# print('OK')
 
 
 # Home work 10.2
@@ -54,3 +42,16 @@ print('OK')
 # assert first_word("hi") == "hi", 'Test5'
 # assert first_word("Hello.World") == "Hello", 'Test6'
 # print('OK')
+
+#Home Work 10.3
+def is_even(digit):
+  # if digit % 2 == 0:
+  #    return True
+  # else:
+  #    return False
+   return digit % 2 == 0
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
