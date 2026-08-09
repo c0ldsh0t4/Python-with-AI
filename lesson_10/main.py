5657ccf0b86356b6712c88b3c9ce0ba9fc6b1d8c
Oleksii Baranovskyi <c0ldsh0t1718@gmail.com>
2026-08-09 10:12:31 +0200

    home work 10.1 is done

--- lesson_10/main.py
+++ lesson_10/main.py
@@ -1,6 +1,20 @@
+#Home work 10.1
+def pow(x):
+   return x ** 2
 
+def some_gen(begin, end, func):
+   yield begin
 
+   for i in range(end -1):
+      begin = func(begin)
+      yield begin
 
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
 
 
 
@@ -16,27 +30,27 @@
 
 
 # Home work 10.2
-def first_word(text):
-   result = text.split()
-   for word in result:
-      word = word.strip(".,")
-      parts = word.split("'")
-      first = word.split(".")
-      word = first[0]
-      if word.isalpha():
-         return word
-      valid = True
-      for part in parts:
-         if not part.isalpha():
-            valid = False
-      if valid:
-         return word
-
-
-assert first_word("Hello world") == "Hello", 'Test1'
-assert first_word("greetings, friends") == "greetings", 'Test2'
-assert first_word("don't touch it") == "don't", 'Test3'
-assert first_word(".., and so on ...") == "and", 'Test4'
-assert first_word("hi") == "hi", 'Test5'
-assert first_word("Hello.World") == "Hello", 'Test6'
-print('OK')
+# def first_word(text):
+#    result = text.split()
+#    for word in result:
+#       word = word.strip(".,")
+#       parts = word.split("'")
+#       first = word.split(".")
+#       word = first[0]
+#       if word.isalpha():
+#          return word
+#       valid = True
+#       for part in parts:
+#          if not part.isalpha():
+#             valid = False
+#       if valid:
+#          return word
+#
+#
+# assert first_word("Hello world") == "Hello", 'Test1'
+# assert first_word("greetings, friends") == "greetings", 'Test2'
+# assert first_word("don't touch it") == "don't", 'Test3'
+# assert first_word(".., and so on ...") == "and", 'Test4'
+# assert first_word("hi") == "hi", 'Test5'
+# assert first_word("Hello.World") == "Hello", 'Test6'
+# print('OK')
